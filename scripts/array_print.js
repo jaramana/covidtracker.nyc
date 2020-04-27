05028bf019fb691a8d7ab189009e5e8fe235670d
jaramana <jaramana_dam@hotmail.com>
2020-04-27 18:43:42 -0400

    2020-04-27 Update
    Daily data update. Moved github button to top, couldn't click becasue of the scroll button.

--- scripts/array_print.js
+++ scripts/array_print.js
@@ -8,11 +8,11 @@ var settings = {
 $.ajax(settings).done(function(response) {
     console.log(response);
 
-    var case_count_a = case_hosp_death_cumulative.jsonarray[53].NEW_COVID_CASE_COUNT_CUM;
+    var case_count_a = case_hosp_death_cumulative.jsonarray[54].NEW_COVID_CASE_COUNT_CUM;
     $("#case_count_a").append(case_count_a);
-    var hospitalized_a = case_hosp_death_cumulative.jsonarray[53].HOSPITALIZED_CASE_COUNT_CUM;
+    var hospitalized_a = case_hosp_death_cumulative.jsonarray[54].HOSPITALIZED_CASE_COUNT_CUM;
     $("#hospitalized_a").append(hospitalized_a);
-    var death_count_a = case_hosp_death_cumulative.jsonarray[53].DEATH_COUNT_CUM;
+    var death_count_a = case_hosp_death_cumulative.jsonarray[54].DEATH_COUNT_CUM;
     $("#death_count_a").append(death_count_a);
 
 });
