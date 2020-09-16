13f0aee7e420769a7ed62564e51567cd305989cd
jaramana <jaramana_dam@hotmail.com>
2020-09-16 18:11:18 -0400

    Revert "Data Revert"
    This reverts commit 45dbb9a2fbe9a42eb1eb471593735206174af2aa.

--- scripts/array_print.js
+++ scripts/array_print.js
@@ -8,19 +8,11 @@ var settings = {
 $.ajax(settings).done(function(response) {
     console.log(response);
 
-<<<<<<< HEAD
     var case_count_a = case_hosp_death_cumulative.jsonarray[199].NEW_COVID_CASE_COUNT_CUM;
     $("#case_count_a").append(case_count_a);
     var hospitalized_a = case_hosp_death_cumulative.jsonarray[199].HOSPITALIZED_CASE_COUNT_CUM;
     $("#hospitalized_a").append(hospitalized_a);
     var death_count_a = case_hosp_death_cumulative.jsonarray[199].DEATH_COUNT_CUM;
-=======
-    var case_count_a = case_hosp_death_cumulative.jsonarray[145].NEW_COVID_CASE_COUNT_CUM;
-    $("#case_count_a").append(case_count_a);
-    var hospitalized_a = case_hosp_death_cumulative.jsonarray[145].HOSPITALIZED_CASE_COUNT_CUM;
-    $("#hospitalized_a").append(hospitalized_a);
-    var death_count_a = case_hosp_death_cumulative.jsonarray[145].DEATH_COUNT_CUM;
->>>>>>> parent of fd62986... Data Update
     $("#death_count_a").append(death_count_a);
 
 });
